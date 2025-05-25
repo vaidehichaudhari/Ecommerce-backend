cf289712e58a32c86c95c68bbff8c1f0a58e2def
Vaidehi <vaidehic48@gmail.com>
2025-05-25 11:35:03 +0530

    added cors for frontend

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const app = express();
-
+const cors = require('cors');
 const { sequelize, connectDB } = require('./confiq/db'); // ✅ Destructure here
 //const Brand = require('./models/brandModel');
 //const Product=require('./models/productModel');
 //const Category=require('./models/categoryModel');
 app.use(express.json());
 
+app.use(cors({
+  origin: 'http://localhost:5173', // Frontend URL
+  credentials: true,               // Needed if using cookies or auth headers
+}));
 
 
 // Routes
@@ -25,7 +29,7 @@ app.use('/api/user', userRoute);
 (async () => {
   try {
     await connectDB(); // optional, but good for testing the connection
-    await sequelize.sync({ alter: true }); 
+    await sequelize.sync({ alter:false }); 
     console.log('Database & tables synced!');
 
     app.listen(7001, () => {
